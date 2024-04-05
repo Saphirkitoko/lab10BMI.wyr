d9c5ee4a0138f7a695b4a96f5a537898376efbbe
Kitoko <qr0304jk@go.minneapolis.edu>
2024-04-04 21:03:03 -0500

    Initial commit

---
+++ lab-10-bmi/src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+// Importing the wouldyourather component from its file
+import WouldYouRather from './components/WouldYouRather.vue'
+import {ref} from 'vue'
+
+// Creating reactive variables for the question and answers
+const wyrQuestion = ref('live in the house shaped like a triangle or a house shaped like like circle')
+const wyrAnswer1 = ref('triangle house')
+const wyrAnswer2 = ref('circle house')
+
+
+
+const userselection = ref('')
+function UpdateUserSelection(userChoice) {
+  userselection.value = `Thanks! you chose ${userChoice}`
+}
+</script>
+
+<template>
+  <div id="app-component">
+<h1>Would You Rather?</h1>
+    <!--rendering the wouldyourather component-->
+  <WouldYouRather
+    v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-selected="updateUserselection"
+    ></WouldYouRather>
+  <p>{{userselection}}</p>
+  </div>
+</template>
+
+<style scoped>
+/*style for the components*/
+#app-component { background-color: aqua;
+padding:40px;}
+p {font-family: fantasy;}
+
+</style>
---
+++ lab-10-bmi/src/components/WouldYouRather.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+// Importing ref and watch functions from vue
+import { ref, watch } from 'vue'
+
+//defining props for the component
+defineProps({
+question:string,
+  answer1:string,
+  answer2:string,
+})
+const emit = defineEmits(['answer-selected'])
+
+const choice =ref("")
+
+watch(choice, () =>{ emit('answer-selected', choice.value)})
+
+</script>
+
+<template>
+  <div id="wyr">
+<h2>make your choice!</h2>
+  <p>{{ question }}</p>
+  <div>
+<!-- radio buttons for selecting answers-->
+  <input v-model="choice" v-bind:value="Answer1" type="radio" id="answer-1">
+  <label for="answer-1">{{ answer1 }}</label>
+  <input v-model="choice" v-bind:value="answer2" type="radio" id="answer-2">
+  <label for="answer-2">  {{ Answer2 }}</label>
+  </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+/* style for component */
+#wyr { background-color: purple;}
+p {font-family: Corbel Light;}
+
+</style>
